4aac6b5167deedafa888bab6a3e3d72db64c2ee3
Igor Zvezdilin <zvezdilin.work@mail.ru>
2020-03-05 22:23:22 +0300

    Make the comments

--- src/com/company/BattleService.java
+++ src/com/company/BattleService.java
@@ -3,6 +3,8 @@ package com.company;
 import java.util.Random;
 
 public class BattleService {
+
+
     public FightLog fight(Units unit1, Units unit2) {
         FightLog fightLog = new FightLog();
         Random random = new Random();
@@ -12,20 +14,20 @@ public class BattleService {
         while (hp1 > 1 & hp2 > 1) {
             fightLog.add(
                     "Round " + round + "\n"
-                            + unit1.getName() + " health points = " + hp1 + "\n"
+                            + unit1.getName() + " health points = " + hp1 + "\n"    //Заносим очки здоровья 1 и 2 юнита в fightLog
                             + unit2.getName() + " health points = " + hp2 + "\n");
-            if (random.nextInt(2) == 1) {
-                switch (random.nextInt(3)) {
-                    case 0:
+            if (random.nextInt(2) == 1) {                                  //Рандомим шанс попадания 2 юнита по 1 юниту
+                switch (random.nextInt(3)) {                              //рандомим количество урона по юниту
+                    case 0:                                                      //наносит весь свой урон
                         hp1 -= unit2.getDamagePoints();
                         fightLog.add(unit2.getName() + " was able to deal " + unit2.getDamagePoints() + " points of damage" + "\n");
                         break;
-                    case 1:
+                    case 1:                                                     //наносит половину урона
                         hp1 -= unit2.getDamagePoints() / 2;
                         fightLog.add(unit2.getName() +
-                                " slipped on a banana peel and was able to deal " + unit2.getDamagePoints() / 2 + " points of damage" + "\n");
+                                " slipped on a banana peel and was able to deal " + unit2.getDamagePoints() / 2 + " points of damage" + "\n"); //причина, почему наносится то или иное количество урона
                         break;
-                    case 2:
+                    case 2:                                                      //наносит четверть урона
                         hp1 -= unit2.getDamagePoints() / 4;
                         fightLog.add(unit2.getName() +
                                 " remembered that he was about to defend a diploma that had not even been written yet, and so he was terribly upset. He was able to deal "
@@ -36,18 +38,18 @@ public class BattleService {
 
             } else fightLog.add(unit2.getName() + " missed");
 
-            if (random.nextInt(2) == 0) {
-                switch (random.nextInt(3)) {
-                    case 0:
+            if (random.nextInt(2) == 0) {            //Рандомим шанс попадания 1 юнита по 2 юниту
+                switch (random.nextInt(3)) {         //рандомим количество урона по юниту
+                    case 0:                                //наносит весь свой урон
                         hp2 -= unit1.getDamagePoints();
                         fightLog.add(unit1.getName() + " was able to deal " + unit1.getDamagePoints() + " points of damage" + "\n");
                         break;
-                    case 1:
+                    case 1:                                //наносит половину урона
                         hp2 -= unit1.getDamagePoints() / 2;
-                        fightLog.add(unit1.getName() + " tripped over a rock and was able to deals "
+                        fightLog.add(unit1.getName() + " tripped over a rock and was able to deals " //причина, почему наносится то или иное количество урона
                                 + unit1.getDamagePoints() / 2 + " points of damage" + "\n");
                         break;
-                    case 2:
+                    case 2:                               //наносит четверть урона
                         hp2 -= unit1.getDamagePoints() / 4;
                         fightLog.add(unit1.getName() +
                                 " suddenly realized that he was just a set of ones and zeros, and so he was terribly upset. He was able to deal "
